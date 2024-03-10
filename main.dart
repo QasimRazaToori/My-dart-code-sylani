8f410d7eb27bbd121925280bbcb402f19dd00b84
Qasim Raza Toori <158839600+QasimRazaToori@users.noreply.github.com>
2024-03-10 08:24:39 +0500

    7.Four important tasks about list.
    I've done for important tasks about list in dart language;
    1. Write a program to calculate the sum of the all elements of the static list.
    2. Write a program that prompts the user to input a positive integer, then develop a list in which it take
    element from user have same length of the user input number.print the list?
    3. Write a program that prompts the user to input a positive integer, then develop a list in which it take
    element from user have same length of the user input number.print the list and find sum of the all elements
    in the list?
    4. Write a program that prompts the user to input a positive integer, then develop a list in which it take
    element from user have same length of the user input number.print the list and find sum of the all elements
    in the list? and then find sum of even number in the list and sum of odd numbers in the list. check wether
    sum of all number and sum of even and odd number is equal or not?

--- main.dart
+++ main.dart
@@ -1,50 +1,143 @@
-// void main() {
-//   List<int> StaticList = [2, 4, 2, 4, 5, 5, 3];
-//   int sum = 0;
-//   for (var i = 0; i < StaticList.length; i++) {
-//     sum = sum + StaticList[i];
-//   }
-//   print('The Sum of the value in list: ${sum}');
-// }
+                                                                            Task1
+ 1. Write a program to calculate the sum of the all elements of the static list.
+
+                                                                              
+  void main() {
+  List<int> staticList = [
+    1,
+    2,
+    3,
+    4,
+  ];
+  int sum = 0;
+  for (var i = 0; i < staticList.length; i++) {
+    sum = sum + staticList[i];
+  }
+  print('The Sum of Static list : ${sum}');
+}
+
+
+                                                                            Task2
+2. Write a program that prompts the user to input a positive integer, then develop a list in which it take
+element from user have same length of the user input number.print the list?
 import 'dart:io';
 
-// void main() {
-//   print('Enter Your Values');
-//   int num = int.parse(stdin.readLineSync()!);
-//   List<dynamic> values = [];
-//   for (var i = 0; i < num; i++) {
-//     int num = int.parse(stdin.readLineSync()!);
-//     values.add(num);
-//   }
-//   print('The List Of The Your Values is ${values}');
-// }
-// void main() {
-//   print('Enter Value please');
-
-//   List<int> list = [];
-//   int len = int.parse(stdin.readLineSync()!);
-//   for (int i = 0; i < len; i++) {
-//     int n = int.parse(stdin.readLineSync()!);
-//     if (n >= 0) {
-//       list.add(n);
-//     } else {
-//       print('Enter Your Number Is Negative');
-//     }
-//     print('List ${list}');
-//     int sum = 0;
-//     for (var j = 0; j < list.length; j++) {}
-//     sum = sum + list.add(j);
-//   }
-//   print(sum);
-// }
 void main() {
-  var map = {
-    'Name': 'Qasim',
-    'roll number': 56,
-    'class': '4th Semester',
-  };
-  print(map);
-
-  print(map.length);
-  print(map.isEmpty);
+  print('Enter Your Number Please!');
+  int num = int.parse(stdin.readLineSync()!);
+  List<int> listnum = [];
+  for (var i = 0; i < num; i++) {
+    int n = int.parse(stdin.readLineSync()!);
+    listnum.add(n);
+  }
+  print("The number in list is : ${listnum}");
 }
+
+                                                                             Task3
+                                                                               
+3. Write a program that prompts the user to input a positive integer, then develop a list in which it take
+element from user have same length of the user input number.print the list and find sum of the all elements
+in the list?
+                                                                               
+  void main() {
+  print('Enter Your Number!');
+  int sum1 = 0;
+  int num = int.parse(stdin.readLineSync()!);
+  List<int> listnum = [];
+  for (int i = 0; i < num; i++) {
+    int n = int.parse(stdin.readLineSync()!);
+    if (n >= 0) {
+      listnum.add(n);
+    }
+  }
+  print("List $listnum");
+  for (var j = 0; j < listnum.length; j++) {
+    sum1 += listnum[j];
+  }
+  print('The Sum Of the Value in a list is : ${sum1}');
+
+} 
+                    
+
+
+                                                               
+                                                               Task4
+4. Write a program that prompts the user to input a positive integer, then develop a list in which it take
+element from user have same length of the user input number.print the list and find sum of the all elements
+in the list? and then find sum of even number in the list and sum of odd numbers in the list. check wether
+sum of all number and sum of even and odd number is equal or not?
+
+import 'dart:io';
+
+void main() {
+  print('Enter Your Number!');
+  int sum1 = 0;
+  int num = int.parse(stdin.readLineSync()!);
+  List<int> listnum = [];
+  for (int i = 0; i < num; i++) {
+    int n = int.parse(stdin.readLineSync()!);
+    if (n >= 0) {
+      listnum.add(n);
+    }
+  }
+  print("List $listnum");
+  for (var j = 0; j < listnum.length; j++) {
+    sum1 += listnum[j];
+  }
+  print('The Sum Of the Value in a list is : ${sum1}');
+
+}
+import 'dart:io';
+
+void main() {
+  List<int> listnum = [];
+  List<int> listoddnum = [];
+  List<int> listevennum = [];
+  int sum1 = 0;
+  int sum2 = 0;
+  int sum3 = 0;
+  int sum4 = 0;
+  print('Enter the number of elements:');
+  int num = int.parse(stdin.readLineSync()!);
+
+  print('Enter $num numbers:');
+  for (int i = 0; i < num; i++) {
+    int n = int.parse(stdin.readLineSync()!);
+    listnum.add(n);
+  }
+
+  print("List: $listnum");
+
+  for (var j = 0; j < listnum.length; j++) {
+    sum1 += listnum[j];
+  }
+
+  print('The Sum Of the Values in the list is: $sum1');
+
+  for (var k = 0; k < listnum.length; k++) {
+    if (listnum[k] % 2 == 0) {
+      listevennum.add(listnum[k]);
+    } else {
+      listoddnum.add(listnum[k]);
+    }
+  }
+  print('Odd numbers in the list: $listoddnum');
+  print('Even numbers in the list: $listevennum');
+  for (int l = 0; l < listevennum.length; l++) {
+    sum2 = sum2 + listevennum[l];
+  }
+  print("The Sum Of Even Number is $sum2");
+
+  for (int o = 0; o < listoddnum.length; o++) {
+    sum3 = sum3 + listoddnum[o];
+  }
+  print("The Sum Of Even Number is $sum3");
+  sum4 = sum2 + sum3;
+
+  if (sum1 == sum4) {
+    print('The Sum OF list:The sum Of both even and odd!');
+  } else {
+    print("Sorry You have Not same value!");
+  }
+}
+
